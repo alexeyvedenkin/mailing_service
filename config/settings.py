ff6d2d7939f54f0e2f919e20560d904f8c460296
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-04 22:18:02 +0500

    инициализация приложения mailwork

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "users.CustomUser"
+# AUTH_USER_MODEL = "users.CustomUser"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
