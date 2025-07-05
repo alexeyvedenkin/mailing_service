fd02a55b420d408eff991f65e218064f03816669
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-05 17:18:20 +0500

    создана модель User, выполнена миграция

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone_number = models.CharField(max_length=15, verbose_name='Телефон', blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True)
+    country = models.CharField(max_length=25, verbose_name='Страна', blank=True, null=True)
+
+    token = models.CharField(max_length=100, verbose_name='Token', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
